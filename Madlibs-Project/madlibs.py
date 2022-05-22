25a79118d784407c23393d2c8edff4480e02a899
Joel V Stan <101164781+JoelVStan@users.noreply.github.com>
2022-05-22 17:56:07 +0530

    A basic Madlibs program

---
+++ Madlibs-Project/madlibs.py
@@ -0,0 +1,18 @@
+# string concatenation the best way
+
+# youtuber = "Joel Stanley"
+
+# print("Subscribe to " + youtuber)
+
+# f-string method - lets use this
+# print(f"Subscribe to {youtuber}")
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"Computer programming is so {adj}!. It makes me so excited all the time because \
+I love to {verb1}. Stay hyderated and {verb2} like you are {famous_person}!"
+
+print(madlib)
